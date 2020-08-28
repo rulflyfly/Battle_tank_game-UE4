95c5f99a8ca0f0cf8494a31dfd91331d72d5fc35
rulflyfly <rulflyfly@gmail.com>
2020-08-28 22:28:35 +0300

    setting up landscape

---
+++ Source/Battle_Tank_Game/Battle_Tank_GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Battle_Tank_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANK_GAME_API ABattle_Tank_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
